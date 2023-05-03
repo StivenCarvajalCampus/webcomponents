8c025670536387b5280874c431131b10df511ec8
StivenCarvajalCampus <stivencarvajal3211@gmail.com>
2023-05-03 14:02:35 -0500

    feat(WebComponents): :sparkles: Components full

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 import "./components/my-tabla.js";
 import"./components/my-header.js";
+import "./components/my-section.js";
+import "./components/my-seleccion.js";
+import "./components/my-aside.js";
+import "./components/my-footer.js";
